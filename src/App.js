7cfba95506876f5ed034bf34a94fb741b68f2834
Aaron Jimenez <notwjimenezaj@gmail.com>
2022-11-18 21:29:16 -0800

    Update create prediction UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { AppBar, Button, Divider, Grid, Paper, SvgIcon, tableBodyClasses, TextField, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Divider, FormControl, Grid, InputAdornment, InputLabel, MenuItem, Paper, Select, SvgIcon, Table, TableBody, tableBodyClasses, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar, Typography } from '@mui/material';
 import { Box, Container } from '@mui/system';
 import { ReactComponent as FlameIcon } from "./flame-icon.svg"
 import { useTheme } from '@emotion/react';
 import { GoogleMap, HeatmapLayer } from '@react-google-maps/api';
 import { useEffect, useState } from 'react';
+import { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 
 function App() {
     
@@ -18,8 +21,11 @@ function App() {
                         <California />
                     </Grid>
 
-                    <Grid item xs={12} lg={3}>
-                        <PredictionInput />
+                    <Grid item xs={12} lg={4}>
+                        <PredictionInput />                      
+                    </Grid>
+
+                    <Grid item xs={12} lg={4}>
                         <PredictionResults />
                     </Grid>
                 </Grid>
@@ -37,7 +43,7 @@ const TopBar = () => {
                         <FlameIcon />
                     </SvgIcon>
 
-                    <Typography sx={{color: "white", weight: 500, fontSize: "1.4rem", textShadow: "2px 2px rgba(0,0,0,0.2)"}}>
+                    <Typography sx={{color: "white", weight: 500, fontSize: "1.4rem", textShadow: "2px 2px 2px rgba(0,0,0,0.2)"}}>
                         California Wildfire Predictions
                     </Typography>
                 </Grid>
@@ -57,6 +63,7 @@ const negRand = () => {
 }
 
 const California = () => {
+    const theme = useTheme();
     const [data, setData] = useState([]);
 
     const groupOffset = {lat: 3.5, lng: 3.5}
@@ -85,97 +92,112 @@ const California = () => {
     }, [])
 
     return (
-        <GoogleMap 
-            mapContainerStyle={{width: "100%", height: "40rem"}}
-            center={{lat: 36.778259, lng: -119.417931}}
-            zoom={6}
-        >  
-            <HeatmapLayer 
-                data={data}
-            />
-        </GoogleMap>
+        <Box sx={{boxShadow: theme.shadows[8]}}>
+            <GoogleMap 
+                mapContainerStyle={{width: "100%", height: "40rem"}}
+                center={{lat: 36.778259, lng: -119.417931}}
+                zoom={6}
+            >  
+                <HeatmapLayer 
+                    data={data}
+                />
+            </GoogleMap>
+        </Box>
     )
 }
 
 const PredictionInput = () => {
     const theme = useTheme();
+    const [date, setDate] = useState(null);
 
     const inputStyles = {
         '& label.Mui-focused': {
-            color: 'rgba(255,255,255,0.8)',
+            color: 'rgba(0,0,0,0.5)',
         },
         
         '& .MuiOutlinedInput-root': {
             '& fieldset': {
-              borderColor: 'rgba(255,255,255,0.5)',
+              borderColor: 'rgba(0,0,0,0.5)',
             },
             '&:hover fieldset': {
-              borderColor: 'rgba(255,255,255,0.8)',
+              borderColor: 'rgba(0,0,0,0.5)',
             },
             '&.Mui-focused fieldset': {
-              borderColor: 'rgba(255,255,255,0.5)',
+              borderColor: 'rgba(0,0,0,0.5)',
             },
         }
     }
 
     return (
-        <Container>
-            <Paper elevation={4} sx={{width: "100%", backgroundColor: theme.palette.primary.main, overflow: "hidden"}}>
-                <Box sx={{marginX: "1rem", marginTop: "0.8rem"}}>
-                    <Typography textAlign="center" sx={{color: "white", fontSize: "1.4rem", fontWeight: 500, textShadow: "2px 2px rgba(0,0,0,0.2)"}}>
+            <Paper elevation={4} sx={{width: "100%", overflow: "hidden"}}>
+                <Box sx={{marginX: "1rem"}}>
+                    <Typography sx={{marginY: "0.8rem", color: "rgb(60,60,60)", fontSize: "1.4rem", lineHeight: "1.2rem", fontWeight: 600, textShadow: "2px 2px 2px rgba(40,40,40,0.2)"}}>
                         Create Prediction
                     </Typography>
 
-                    <Divider sx={{marginY: "1rem"}} />
+                    <Divider sx={{marginY: "0.8rem"}} />
 
-                    <TextField 
-                        label="Date" 
-                        size="small" 
-                        fullWidth 
-                        InputProps={{sx: {boxShadow: theme.shadows[3]}}}
-                        InputLabelProps={{sx: {color: "white"}}} 
-                        sx={{
-                            marginBottom: "1rem",
-                            ...inputStyles
-                        }} 
-                    />
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="Date"
+                            value={date}
+                            onChange={(newValue) => setDate(newValue)}
+                            renderInput={(params) => <TextField fullWidth {...params} sx={{marginBottom: "0.8rem"}} />}
+                        />
+                    </LocalizationProvider>
 
-                    <TextField 
-                        label="Rainfall" 
-                        size="small" 
-                        fullWidth 
-                        InputProps={{sx: {boxShadow: theme.shadows[3]}}}
-                        InputLabelProps={{sx: {color: "white"}}} 
-                        sx={{
-                            marginBottom: "1rem",
-                            ...inputStyles
-                        }} 
-                    />
+                    <Grid container spacing={2} sx={{marginBottom: "0.8rem"}}>
+                        <Grid item xs={8}>
+                            <TextField 
+                                fullWidth
+                                label="Rainfall"
+                                type="number"
+                                InputProps={{
+                                    endAdornment: <InputAdornment position="end">in</InputAdornment>,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <FormControl fullWidth>
+                                <InputLabel id="timespan-select-label">Timespan</InputLabel>
+                                <Select
+                                    labelId="timespan-select-label"
+                                    label="Timespan"
+                                >
+                                    <MenuItem value={1}>1 month</MenuItem>
+                                    <MenuItem value={2}>2 months</MenuItem>
+                                    <MenuItem value={3}>3 months</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+
+                    <Divider sx={{marginY: "0.8rem"}} />
 
                     <Button 
-                        fullWidth
-                        variant='contained'
+                        variant='contained' 
                         sx={{
-                            marginBottom: "1rem", 
                             textTransform: "none",
-                            backgroundColor: "rgba(250,209,92,1)",
-                            borderStyle: "solid",
-                            borderWidth: "1px",
-                            borderColor: "rgba(236,111,89,0.6)",
+                            borderRadius: "12px",
                             boxShadow: theme.shadows[4],
-                            "&:hover": {
-                                backgroundColor: "rgba(230,191,86,1)",
-                                boxShadow: theme.shadows[1]
-                            }
+                            marginBottom: "1rem"
                         }}
                     >
-                        <Typography sx={{fontSize: "1.2rem", lineHeight: "1.4rem", textShadow: "2px 2px rgba(236,111,89,0.2)", fontWeight: 600}}>
-                            Predict!
-                        </Typography>
+                        <Container>
+                            <Grid container sx={{marginRight: "0.4rem"}}>
+                                <SvgIcon sx={{width: "1.6rem", height: "1.6rem", marginRight: "0.4rem"}}>
+                                    <FlameIcon />
+                                </SvgIcon>
+
+                                <Typography sx={{lineHeight: "1.6rem", fontSize: "1.4rem", color: "white", fontWeight: 600, textShadow: "2px 2px 2px rgba(0,0,0,0.4)"}}>
+                                    Predict
+                                </Typography>
+                            </Grid>
+                        </Container>
                     </Button>
                 </Box>
             </Paper>
-        </Container> 
+      
     )
 }
 
@@ -207,12 +229,12 @@ const PredictionResults = () => {
     }, [])
 
     return (
-        <Container>
+    
             <Box
                 sx={{
+                    width: "100%",
                     maxHeight: "40rem",
                     overflow: "auto",
-                    marginTop: "1rem",
                     borderRadius: "0.5rem",
                     borderStyle: "solid",
                     borderWidth: "1px",
@@ -255,8 +277,27 @@ const PredictionResults = () => {
                 </Container>
                 
             </Box>
-        </Container>
+    
         
+    )
+}
+
+const History = () => {
+    return (
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Fires/Year</TableCell>
+                        <TableCell>Fires/Year</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
